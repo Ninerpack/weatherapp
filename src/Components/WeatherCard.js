eb4871cdd39ed43732bc6237609565dd659d073b
Arnie Hayes <arniehayes1@gmail.com>
2021-10-04 21:37:31 -0700

    Added cards based on array objects

--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -1,36 +1,39 @@
 import React from 'react'
 
-let cities = [
-  {
-    name: "Santa Rosa",
-    temp: 67,
-    weather_type: "Cloudy",
-  },
-  {
-    name: "San Fransisco",
-    temp: 55,
-    weather_type: "Cloudy",
-  },
-  {
-    name: "Los Angeles",
-    temp: 92,
-    weather_type: "Sunny",
-  },
-  {
-    name: "San Diego",
-    temp: 70,
-    weather_type: "Sunny",
-  }
-];
 
 function WeatherCard() {
+  // this will be replaced with API data
+  let cities = [
+    {
+      name: "Santa Rosa",
+      temp: 67,
+      weather_type: "Cloudy",
+    },
+    {
+      name: "San Fransisco",
+      temp: 55,
+      weather_type: "Cloudy",
+    },
+    {
+      name: "Los Angeles",
+      temp: 92,
+      weather_type: "Sunny",
+    },
+    {
+      name: "San Diego",
+      temp: 70,
+      weather_type: "Sunny",
+    },
+  ];
     return (
       <div className="ContainerCard">
-        <div className="Card">
-            {cities.map((city, idx) => {
-                return <div className="CityName" key={idx}>{city.name}</div>
-            })}
-        </div>
+          {cities.map((city, index) => (
+              <div className="Card">
+                <p className='Name' key={ index }>{ city.name }</p>
+                <p className='Temp' key={ index }>{ city.temp }</p>
+                <p className='Type' key={ index }>{ city.weather_type }</p>
+              </div>
+            ))}
       </div>
     );
 }

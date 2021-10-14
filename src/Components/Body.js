08f480a08383445b01107c1c4733978dfe0ada2c
Arnie Hayes <arniehayes1@gmail.com>
2021-10-13 22:42:30 -0700

    rendering cards with a button

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import WeatherCard from './WeatherCard'
+import { useState } from 'react'
 
 const Body = () => {
+
+  const [card, setCard] = useState(false);
+
+  const addCard = () => {
+    setCard(true)
+    console.log(card);
+    //return <WeatherCard />
+  }
+
   return (
-    <div className='Body'>
-      <div className='Add_location'>
-        <button className='add_button'> Add Location</button>
+    <div className="Body_container">
+      <div className="Add_location">
+        <button className="add_button" onClick={() => addCard()}> Add Location</button>
       </div>
-      <WeatherCard />
+      <div className='Body'>
+        {!card ? <p>No locations</p> : <WeatherCard />}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Body

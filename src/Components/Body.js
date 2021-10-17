b9cd5ab1829f090d579f96e15690f8244c0f37bf
Arnie Hayes <arniehayes1@gmail.com>
2021-10-17 16:55:16 -0700

    redesign

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,20 +1,38 @@
-import React from 'react'
-import WeatherCard from './WeatherCard'
+import React, { useContext } from "react";
 import { useState } from 'react'
+// importing the context
+import { AddressContext } from "../App";
 
 const Body = () => {
-  const [cardCounter, setCardCounter] = useState(0);
+  // pulling the state from the useState
+  const { city, temp, condition } = useContext(AddressContext);
 
   return (
     <div className="Body_container">
-      <div className="Add_location">
-        <button className="add_button" onClick={() => setCardCounter(current => current + 1)} id="card">
-          Add Location
-        </button>
-      </div>
       <div className="Body" id="body">
-        <div className="ContainerCard">
-          {[...Array(cardCounter)].map((card, index) => <WeatherCard key={index}/>)}
+        <div className="Daily-info">
+          <div className="City">{city}</div>
+          <div className="Temp">{temp}</div>
+          <div className="Condition">{condition}</div>
+        </div>
+        <div className="Info-container">
+          <div className="Weekly-info">
+            <div className="Card-small">{"Sun"}</div>
+            <div className="Card-small">{"Mon"}</div>
+            <div className="Card-small">{"Tue"}</div>
+            <div className="Card-small">{"Wed"}</div>
+            <div className="Card-small">{"Thur"}</div>
+            <div className="Card-small">{"Fri"}</div>
+            <div className="Card-small">{"Sat"}</div>
+          </div>
+          <div className="Other-info">
+            <div className="Card-big">{""}</div>
+            <div className="Card-big">{""}</div>
+            <div className="Card-big">{""}</div>
+            <div className="Card-big">{""}</div>
+            <div className="Card-big">{""}</div>
+            <div className="Card-big">{""}</div>
+          </div>
         </div>
       </div>
     </div>

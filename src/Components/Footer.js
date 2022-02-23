7402efb77b1844a382e0f7e15b951120aef9e5c6
Arnie Hayes <arniehayes1@gmail.com>
2022-02-22 17:34:32 -0800

    asfaf

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='footer_container'>
             <div className='footer_icons'>
-                <div className='icons'> icon </div>
+                <div className='icons'> Data provided by Open Weather Map </div>
             </div>
         </div>
     )
